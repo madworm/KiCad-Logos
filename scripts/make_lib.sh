5342686919b3233fc4fff378ec7d74257ba6f015
Robert Spitzenpfeil <robert@linuxbox.site>
2013-01-30 20:27:35 +0100

    'make_lib.sh' now supports sequences like: <start> <increment> <end>
     0  5 20  -->   5, 10, 15, 20
    10 10 30  -->  10, 20, 30

--- scripts/make_lib.sh
+++ scripts/make_lib.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-if [[ $# -ne 5 ]]
+if [[ $# -ne 6 ]]
 then
-	echo -e "\nusage: $0 input-file.emp library-name.mod <start> <end> <h-symmetric?>."
-	echo -e "\n  'start' and 'end' are integer (1,2,3...) sizes in mm (metric)."
-	echo -e "\n  Images with horizontal symmetry don't need to be mirrored for back-layers. Answer with 'y' or 'n'.\n"
+	echo -e "\n  usage: $0 input-file.emp library-name.mod <start> <increment> <end> <h-sym: y/n>."
+	echo -e "\n         <start>, <increment> and <end> are integer (1,2,3...) sizes in mm (metric)."
+	echo -e "\n         Images with horizontal symmetry don't need to be mirrored for back-layers. Answer with 'y' or 'n'."
+	echo -e "\n\n  Example 1: create a library with sizes of 5, 10, 15 and 20mm for a h-symmetric logo."
+	echo -e "\n             $0 my-logo.emp my-logo.mod 0 5 20 y\n"
+	echo -e "\n  Example 2: create a library with sizes of 10, 20, and 30mm for a non-h-symmetric logo."
+	echo -e "\n             $0 my-logo.emp my-logo.mod 10 10 30 n\n"
 	exit
 fi
 
 INPUT_FILE=$1
 LIB_NAME=$2
 RANGE_START=$3
-RANGE_END=$4
+RANGE_INCREMENT=$4
+RANGE_END=$5
 LIB_FOLDER="./lib"
-IMAGE_IS_H_SYMMETRIC=$5
+IMAGE_IS_H_SYMMETRIC=$6
 
 if [[ ! -d $LIB_FOLDER ]]
 then
@@ -27,14 +32,14 @@ TMP_FILE=`mktemp`
 
 if [[ $IMAGE_IS_H_SYMMETRIC = "y" ]]
 then
-	for number in `seq $RANGE_START $RANGE_END`
+	for number in `seq $RANGE_START $RANGE_INCREMENT $RANGE_END`
 	do
 	        ./scale.pl $INPUT_FILE $TMP_FILE 21 ${number}.0mm
 		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_silkscreen_${number}mm/" $TMP_FILE
 		cat $TMP_FILE >> ./$LIB_NAME
 	done
 
-	for number in `seq $RANGE_START $RANGE_END`
+	for number in `seq $RANGE_START $RANGE_INCREMENT $RANGE_END`
 	do
 	       	./scale.pl $INPUT_FILE $TMP_FILE 15 ${number}.0mm
 		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_copper_${number}mm/" $TMP_FILE
@@ -42,7 +47,7 @@ then
 	done
 
 else
-	for number in `seq $RANGE_START $RANGE_END`
+	for number in `seq $RANGE_START $RANGE_INCREMENT $RANGE_END`
 	do
 	        ./scale.pl $INPUT_FILE $TMP_FILE 21 ${number}.0mm
 		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_silkscreen-front_${number}mm/" $TMP_FILE
@@ -53,7 +58,7 @@ else
 		cat $TMP_FILE >> ./$LIB_NAME
 	done
 
-	for number in `seq $RANGE_START $RANGE_END`
+	for number in `seq $RANGE_START $RANGE_INCREMENT $RANGE_END`
 	do
 	       	./scale.pl $INPUT_FILE $TMP_FILE 15 ${number}.0mm
 		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_copper-front_${number}mm/" $TMP_FILE
