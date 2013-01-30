0d680a31499658bfac93a1aef67c6e5e9f68f8b6
Robert Spitzenpfeil <robert@linuxbox.site>
2013-01-30 18:45:25 +0100

    Made 'make_lib.sh' aware of horizontal image symmetry (user input).
    Images with horizontal symmetry don't have to be mirrored when put on
    back-layers. This potentially cuts the library size in half.

--- scripts/make_lib.sh
+++ scripts/make_lib.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [[ $# -ne 4 ]]
+if [[ $# -ne 5 ]]
 then
-	echo -e "\nusage: $0 input-file.emp library-name.mod <start> <end>. 'start' and 'end' are integer (1,2,3...) sizes in mm (metric)\n"
+	echo -e "\nusage: $0 input-file.emp library-name.mod <start> <end> <h-symmetric?>."
+	echo -e "\n  'start' and 'end' are integer (1,2,3...) sizes in mm (metric)."
+	echo -e "\n  Images with horizontal symmetry don't need to be mirrored for back-layers. Answer with 'y' or 'n'.\n"
 	exit
 fi
 
@@ -11,6 +13,7 @@ LIB_NAME=$2
 RANGE_START=$3
 RANGE_END=$4
 LIB_FOLDER="./lib"
+IMAGE_IS_H_SYMMETRIC=$5
 
 if [[ ! -d $LIB_FOLDER ]]
 then
@@ -22,27 +25,48 @@ echo -e "\n working...\n"
 
 TMP_FILE=`mktemp`
 
-for number in `seq $RANGE_START $RANGE_END`
-do
-        ./scale.pl $INPUT_FILE $TMP_FILE 21 ${number}.0mm
-	perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_silkscreen-front_${number}mm/" $TMP_FILE
-	cat $TMP_FILE >> ./$LIB_NAME
+if [[ $IMAGE_IS_H_SYMMETRIC = "y" ]]
+then
+	for number in `seq $RANGE_START $RANGE_END`
+	do
+	        ./scale.pl $INPUT_FILE $TMP_FILE 21 ${number}.0mm
+		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_silkscreen_${number}mm/" $TMP_FILE
+		cat $TMP_FILE >> ./$LIB_NAME
+	done
 
-        ./scale.pl $INPUT_FILE $TMP_FILE 20 ${number}.0mm
-	perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_silkscreen-back_${number}mm/" $TMP_FILE
-	cat $TMP_FILE >> ./$LIB_NAME
-done
+	for number in `seq $RANGE_START $RANGE_END`
+	do
+	       	./scale.pl $INPUT_FILE $TMP_FILE 15 ${number}.0mm
+		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_copper_${number}mm/" $TMP_FILE
+		cat $TMP_FILE >> ./$LIB_NAME
+	done
 
-for number in `seq $RANGE_START $RANGE_END`
-do
-       	./scale.pl $INPUT_FILE $TMP_FILE 15 ${number}.0mm
-	perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_copper-front_${number}mm/" $TMP_FILE
-	cat $TMP_FILE >> ./$LIB_NAME
+else
+	for number in `seq $RANGE_START $RANGE_END`
+	do
+	        ./scale.pl $INPUT_FILE $TMP_FILE 21 ${number}.0mm
+		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_silkscreen-front_${number}mm/" $TMP_FILE
+		cat $TMP_FILE >> ./$LIB_NAME
 
-	./scale.pl $INPUT_FILE $TMP_FILE 0 ${number}.0mm
-	perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_copper-back_${number}mm/" $TMP_FILE
-	cat $TMP_FILE >> ./$LIB_NAME
-done
+	        ./scale.pl $INPUT_FILE $TMP_FILE 20 ${number}.0mm
+		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_silkscreen-back_${number}mm/" $TMP_FILE
+		cat $TMP_FILE >> ./$LIB_NAME
+	done
+
+	for number in `seq $RANGE_START $RANGE_END`
+	do
+	       	./scale.pl $INPUT_FILE $TMP_FILE 15 ${number}.0mm
+		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_copper-front_${number}mm/" $TMP_FILE
+		cat $TMP_FILE >> ./$LIB_NAME
+
+		./scale.pl $INPUT_FILE $TMP_FILE 0 ${number}.0mm
+		perl -pi -e "s/LOGO/${LIB_NAME/%.mod/}_copper-back_${number}mm/" $TMP_FILE
+		cat $TMP_FILE >> ./$LIB_NAME
+	done
+fi
+
+
+
 
 echo -e "\n cleaning up... removed $TMP_FILE\n"
 rm $TMP_FILE
